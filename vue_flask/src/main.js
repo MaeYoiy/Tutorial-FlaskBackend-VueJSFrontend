7d776a10047d98e67278b5ce3c4f61ab3849ad25
MaeYoiy <b6333034@gmail.com>
2023-09-11 21:55:05 +0700

    finally

--- vue_flask/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '../src/resources/css/bootstrap.css'
-
-
-createApp(App).use(router).mount('#app')
